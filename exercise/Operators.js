3d2c52b5291a88d3fad025cac110b65d36eba0f9
mehedi369 <mdabdullahalmehedi@gmail.com>
2018-08-20 15:19:19 +0600

    JavaScript operators.

---
+++ exercise/Operators.js
@@ -0,0 +1,19 @@
+/*		JavaScript Operators
+	----------------------------
+1. JavaScript Arithmetic Operators :
+	-> Arithmetic Operators are used for  do arithmetic on 
+numbers. We can perform arithmetical operatiins like +, -, *, /, 
+%, **, --, ++.
+2. JavaScript Assignment Operators :
+	-> Assignment operator is used for assigning value to a 
+variable. 
+3. JavaScript String Operators :
+4. JavaScript Comparison Operators :
+5. JavaScript Logical Operators :
+6. JavaScript Type Operators :
+7. JavaScript Bitwise Operators :
+*/
+
+// Exoponent - Arithmetic operator
+console.log(8 ** 8);
+// 
